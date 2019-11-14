603651b190cb1bc7a726f86b632642a885d82eab
Bojan Komazec <bojan.komazec@gmail.com>
2019-11-14 13:08:23 +0000

    Added testing demo module in order to examine passing cli flags to tests.

---
+++ internal/pkg/testingdemo/sut.go
@@ -0,0 +1,5 @@
+package testingdemo
+
+func add(n1, n2 int) int {
+	return n1 + n2
+}
---
+++ internal/pkg/testingdemo/sut_test.go
@@ -0,0 +1,42 @@
+package testingdemo
+
+import (
+	"flag"
+	"log"
+	"os"
+	"testing"
+)
+
+var (
+	aFlag = flag.String("f", "", "a flag")
+)
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	if *aFlag == "" {
+		log.Fatal("shouldn't be empty in main")
+	} else {
+		log.Printf("aFlag = %s", *aFlag)
+	}
+
+	log.Println("Do stuff BEFORE the tests!")
+	exitVal := m.Run()
+	log.Println("Do stuff AFTER the tests!")
+
+	os.Exit(exitVal)
+}
+
+func TestA(t *testing.T) {
+	log.Println("TestA running")
+}
+
+func TestB(t *testing.T) {
+	log.Println("TestB running")
+}
+
+func TestAdd(t *testing.T) {
+	if *aFlag == "" {
+		t.Fatal("shouldn't be empty in tests")
+	}
+}
